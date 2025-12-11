0774894c6ef5b8d18ba71ee7b93cc02d0f9e0902
annauafalafif <144654253+annauafalafif@users.noreply.github.com>
2025-12-11 08:53:40 +0700

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  // config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);  // change importing css to less
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {
+      '@primary-color': '#18A55C',
+      '@font-size-base': '14px'
+    },
+  })(config, env);
+  return config;
+};
